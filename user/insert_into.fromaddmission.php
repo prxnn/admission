5aaccc9995d29afcb279d2fb86ee42f86b082ce1
prxnn <your_email@example.com>
2025-05-18 14:28:32 +0700

    1805

--- user/insert_into.fromaddmission.php
+++ user/insert_into.fromaddmission.php
@@ -3,4 +3,11 @@ include("../conn.php");
 $sex = $_POST["sex"];
 $first_name = $_POST["first_name"];
 $last_name = $_POST["last_name"];
+$dob = $_POST["dob"];
+$gender = $_POST["gender"];
+$national_id = $_POST["national_id"];
+$phone = $_POST["phone"];
+$address = $_POST["address"];
+$email = $_POST["email"];
+
 ?>
